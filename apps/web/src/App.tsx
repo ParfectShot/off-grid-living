06b8f5e341975f8f64442292798e6d94ce50eaf0
Paritosh <38669242+ParfectShot@users.noreply.github.com>
2025-03-25 14:57:03 +0400

    Update App.tsx

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,9 +1,15 @@
 import { RouterProvider } from '@tanstack/react-router'
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 import { createRouter } from './router'
 
 // Create the router instance
 const router = createRouter()
 
 export function App() {
-  return <RouterProvider router={router} />
+  return <>
+    <RouterProvider router={router} />
+    <SpeedInsights />
+    <Analytics mode="production" />
+  </>
 }
